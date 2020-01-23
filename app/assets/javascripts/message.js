1bd4602a0e344e7794091ce5172f81d169d18e3c
atsuya-koshida <koshi0845@gmail.com>
2020-01-23 16:06:34 +0900

    auto-fix

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,29 +1,5 @@
 $(function(){ 
-      var reloadMessages = function() {
-        last_message_id = $('.message:last').data("message-id");
-        $.ajax({
-          url: "api/messages",
-          type: 'get',
-          dataType: 'json',
-          data: {id: last_message_id}
-        })
-        .done(function(messages) {
-          if (messages.length !== 0) {
-            var insertHTML = '';
-            $.each(messages, function(i, message) {
-              insertHTML += buildHTML(message)
-            });
-            $('.messages').append(insertHTML);
-            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-            $("#new_message")[0].reset();
-            $(".form__submit").prop("disabled", false);
-          }
-        })
-        .fail(function() {
-          alert("自動更新に失敗しました");
-        });
-      };
-      function buildHTML(message){
+    function buildHTML(message){
       if ( message.image ) {
         var html =
           `<div class="message" data-message-id=${message.id}>
@@ -63,6 +39,31 @@ $(function(){
         return html;
       };
     }
+    var reloadMessages = function() {
+      last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.messages').append(insertHTML);
+          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+          $("#new_message")[0].reset();
+          $(".form__submit").prop("disabled", false);
+        }
+      })
+      .fail(function() {
+        alert("自動更新に失敗しました");
+      });
+    };
+      
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
