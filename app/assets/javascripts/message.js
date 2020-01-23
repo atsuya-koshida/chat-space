8409d3b9ff6c7d8f48948756b51afce9dbc8864a
atsuya-koshida <koshi0845@gmail.com>
2020-01-23 15:44:42 +0900

    auto-reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,46 +1,68 @@
 $(function(){ 
-  last_message_id = $('.message:last').data("message-id");
-  console.log(last_message_id);
-  function buildHTML(message){
-  if ( message.image ) {
-    var html =
-      `<div class="message" data-message-id=${message.id}>
-        <div class="message__upper-info">
-          <div class="message__upper-info--talker">
-            ${message.user_name}
-          </div>
-          <div class="message__upper-info--date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="message__text">
-          <p class="message__text__content">
-            ${message.content}
-          </p>
-        </div>
-        <img src=${message.image} >
-      </div>`
-    return html;
-  } else {
-    var html =
-      `<div class="message" data-message-id=${message.id}>
-        <div class="message__upper-info">
-          <div class="message__upper-info--talker">
-            ${message.user_name}
-          </div>
-          <div class="message__upper-info--date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="message__text">
-          <p class="message__text__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-    return html;
-  };
-}
+      var reloadMessages = function() {
+        last_message_id = $('.message:last').data("message-id");
+        $.ajax({
+          url: "api/messages",
+          type: 'get',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
+        .done(function(messages) {
+          if (messages.length !== 0) {
+            var insertHTML = '';
+            $.each(messages, function(i, message) {
+              insertHTML += buildHTML(message)
+            });
+            $('.messages').append(insertHTML);
+            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+            $("#new_message")[0].reset();
+            $(".form__submit").prop("disabled", false);
+          }
+        })
+        .fail(function() {
+          alert("自動更新に失敗しました");
+        });
+      };
+      function buildHTML(message){
+      if ( message.image ) {
+        var html =
+          `<div class="message" data-message-id=${message.id}>
+            <div class="message__upper-info">
+              <div class="message__upper-info--talker">
+                ${message.user_name}
+              </div>
+              <div class="message__upper-info--date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message__text">
+              <p class="message__text__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+          `<div class="message" data-message-id=${message.id}>
+            <div class="message__upper-info">
+              <div class="message__upper-info--talker">
+                ${message.user_name}
+              </div>
+              <div class="message__upper-info--date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message__text">
+              <p class="message__text__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -64,4 +86,7 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
   })
 })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+}
 });
